cfa3c5a61e2d112e3c629023d7c533d5045388a4
Niko Caron <niko.caron90@gmail.com>
2018-02-05 22:44:49 -0400

    Finish styling password sent page

--- src/Pages/PasswordSent/style.js
+++ src/Pages/PasswordSent/style.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+import * as queries from '../../Common/queries';
 import {
   charcoal_grey,
   cool_grey
@@ -28,6 +29,34 @@ const PasswordSentContainer = styled.div`
   button {
     margin-right: auto;
     margin-left: auto;
+  }
+
+  @media (min-width: ${queries.MEDIUM}) {
+    h1 {
+      font-size: 28px;
+      line-height: 1.71;
+    }
+
+    p {
+      font-size: 18px;
+      line-height: 1.33;
+    }
+  }
+
+  @media (min-width: ${queries.LARGE}) {
+    position: absolute;
+    top: 425px;
+    left: 719px;
+    text-align: left;
+
+    h1 {
+      span { display: inline; }
+    }
+
+    button {
+      margin-right: 0;
+      margin-left: 0;
+    }
   }
 `;
 
